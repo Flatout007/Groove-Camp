49300eacb011f156b1ad513984b8cce076ff0ea7
Nightwatch <rdmc0072@yahoo.com>
2021-04-06 09:10:55 -0700

    refactored user auth, added controllers

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,18 @@
+class Api::UsersController < ApplicationController
+    def new
+    end
+
+    def create
+        @user = User.find_by(params.require(:user).permit(:username, :password))
+
+        if @user.save === false 
+            flash[:errors] = ['Something went wrong']
+            render(:new)
+         else
+           redirect_to(users_url())
+        end
+    end
+
+    def destroy
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ApplicationRecord
     validates(:password, length:{minimum: 6, allow_nil: true})
     validates(:artist_check, inclusion:{ in: [true,false] })
 
+    attr_reader(:password)
 
-    def self.findByCredentials(username)
+    after_initialize(:ensure_session_token) 
+
+    def self.find_by_credentials(username,password)
         user = User.find_by(username: username)
         if user && BCrypt::Password.new(user.password_digest).is_password?(password)
             return user
