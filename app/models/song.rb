dc810dd2094eef3ea0b9e7be7142949627848da9
Nightwatch <rdmc0072@yahoo.com>
2021-04-16 07:31:04 -0700

    seeding

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,18 +4,18 @@ class Song < ApplicationRecord
 
     has_one_attached :audio
 
-    belongs_to(
-        :artist,
-        foreign_key: :artist_id,
-        class_name: 'User',
-        optional: true
-    )
+    # belongs_to(
+    #     :artist,
+    #     foreign_key: :artist_id,
+    #     class_name: 'User',
+    #     optional: true
+    # )
 
-    belongs_to(
-        :album,
-        foreign_key: :album_id,
-        class_name: 'Album',
-        optional: true
-    )
+    # belongs_to(
+    #     :album,
+    #     foreign_key: :album_id,
+    #     class_name: 'Album',
+    #     optional: true
+    # )
 
 end
