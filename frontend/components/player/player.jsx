9fbad294ff4ec5515261458c7d8da5e71d848311
flatout007 <thechamp301@gmail.com>
2021-05-26 12:29:57 -0700

    added scrubbing functionality to seekbar

--- frontend/components/player/player.jsx
+++ frontend/components/player/player.jsx
@@ -5,13 +5,13 @@ import ReactDOM from 'react-dom';
 class Player extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {playing: false, trackSwitch: false, percent: 0 };
+        this.state = {playing: false, trackSwitch: false, percent: 0, mousedown: false };
         this.handlePlayPause = this.handlePlayPause.bind(this);
         this.audio = null;
-        // this.progressBar = null;
         this.handleAudioSelection = this.handleAudioSelection.bind(this);
         this.handleNext = this.handleNext.bind(this);
         this.handleSeekBar = this.handleSeekBar.bind(this);
+        this.handleSrcubbing = this.handleSrcubbing.bind(this);
     }
 
 
@@ -25,9 +25,6 @@ class Player extends React.Component {
         const action = this.audio.paused ? 'play' : 'pause';
         this.setState({playing: this.audio.paused ? true : false});
         this.audio[action]();
-        // this.handleSeekBar();
-        // const audioSource = this.audio.querySelector('source');
-        // console.log(audioSource)
     }
 
     handleNext() {
@@ -41,16 +38,24 @@ class Player extends React.Component {
     }
 
     handleTrackChange(track) {
-        // if (playing) trackSwitch = true;
         if(this.state.playing) this.setState({trackSwitch: true});
-
         this.audio = this.handleAudioSelection();
         let audioSource = this.audio.querySelector('source');
         audioSource.setAttribute('src', track.audioUrl);
         audioSource.dataset.trackid = this.props.songs.indexOf(track);
         this.audio.load();
 
+
         if(this.state.playing) this.audio.play();
+    }
+
+    handleSrcubbing(e) {
+        e.preventDefault();
+        // e.stopPropagation()
+        this.audio = this.handleAudioSelection();
+        let progress = document.querySelector('.progress');
+        const scrubTime = (e.nativeEvent.offsetX / progress.offsetWidth) * this.audio.duration;
+        this.audio.currentTime = scrubTime;
     }
 
 
@@ -65,6 +70,7 @@ class Player extends React.Component {
     render() {
         if(!this.props.songs[0]) return null;
         console.log(this.state);
+        // let mousedown = false;
     
 
         return(
@@ -73,22 +79,23 @@ class Player extends React.Component {
                         {/* <source src="https://www.bensound.com/bensound-music/bensound-ukulele.mp3" type="audio/mpeg" data-trackid="1"/> */}
                         <source src={this.props.songs[0].audioUrl} type="audio/mpeg" data-trackid="0" />
                 </audio>
-                <div className="song-panel">
                         <div className="song-info">
-                                <div className="song-info__title">{this.props.songs[0].title}</div>
-                                {/* <div className="song-info__artist">Bensound</div> */}
-                                <div className="progress">
-                                <div className="progress__filled"></div>
-                                </div>
+                        <div  className="song-info__title">{this.props.songs[0].title}</div>
+
                         </div>
+                <div onMouseDown={() => this.setState({ mousedown: true })} onMouseUp={() => this.setState({ mousedown: false })}  onClick={this.handleSrcubbing} onMouseMove={(e) => this.state.mousedown && this.handleSrcubbing(e)} className="progress">
+                    <div  className="progress__filled">
+                    </div>
+                    <a  className='thumb'></a>
                 </div>
                 <div className="player-controls">
-                        <button className="backward"><i className="fas fa-backward fa-2x"></i></button>
+                        <button className="backward">{/* make icon for back button */}</button>
                         <div onClick={this.handlePlayPause} className="play-container">
                                 <li className="play"></li>
                         </div>  
-                        <button className='pause'></button>
-                        <button className="forward" onClick={this.handleNext}>  next </button>
+                        {/* <button className='pause'></button> */}
+                    <div className="forward"><p>&#9134;</p><p>◀</p><p>◀</p></div>
+                    <button  onClick={this.handleNext}></button>
                 </div>
 </div>)
     }
